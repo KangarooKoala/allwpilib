8c84085dfa7abc426b17f016f0e9a7cb7ebd28ac
Joseph Eng <91924258+KangarooKoala@users.noreply.github.com>
2024-10-10 19:11:48 +0000

    Don't sort the buffer

--- wpimath/src/test/native/cpp/TimeTest.cpp
+++ wpimath/src/test/native/cpp/TimeTest.cpp
@@ -96,7 +96,6 @@ void ProcessDurations(const wpi::array<units::nanosecond_t, N>& durations,
   wpi::print("{}Last 10: {}\n", prefix, buffer);
 
   wpi::array<units::nanosecond_t, N> sorted{durations};
-  std::sort(sorted.begin(), sorted.end());
 }
 
 template <size_t N>
