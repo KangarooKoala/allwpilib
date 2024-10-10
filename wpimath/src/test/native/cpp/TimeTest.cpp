e723eb68010840b89b551ce48c9362a4e2f43f6a
Joseph Eng <91924258+KangarooKoala@users.noreply.github.com>
2024-10-10 18:22:25 +0000

    Remove fastest buffer

--- wpimath/src/test/native/cpp/TimeTest.cpp
+++ wpimath/src/test/native/cpp/TimeTest.cpp
@@ -88,15 +88,6 @@ void ProcessDurations(const wpi::array<units::nanosecond_t, N>& durations,
   }
 
   wpi::print("{}First 10: {}\n", prefix, buffer);
-
-  wpi::array<units::nanosecond_t, N> sorted{durations};
-  std::sort(sorted.begin(), sorted.end());
-
-  for (size_t i = 0; i < 10; ++i) {
-    buffer[i] = sorted[i];
-  }
-
-  wpi::print("{}Fastest 10: {}\n", prefix, buffer);
 }
 
 template <size_t N>
