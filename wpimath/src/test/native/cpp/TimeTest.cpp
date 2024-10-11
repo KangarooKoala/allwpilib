1cbcf8f50011e62f1a751fb65d10dcff4dba53c4
Joseph Eng <91924258+KangarooKoala@users.noreply.github.com>
2024-10-11 01:12:12 +0000

    Change array sizes

--- wpimath/src/test/native/cpp/TimeTest.cpp
+++ wpimath/src/test/native/cpp/TimeTest.cpp
@@ -96,12 +96,12 @@ void ProcessDurations(const wpi::array<units::nanosecond_t, N>& durations,
   wpi::print("{}Last 10: {}\n", prefix, buffer);
   std::fflush(stdout);
 
-  wpi::array<units::nanosecond_t, 10> array_a(wpi::empty_array);
-  for (size_t i = 0; i < 10; ++i) {
+  wpi::array<units::nanosecond_t, N> array_a(wpi::empty_array);
+  for (size_t i = 0; i < N; ++i) {
     array_a[i] = 0_ns;
   }
 
-  [[ maybe_unused ]] std::array<units::nanosecond_t, 10> array_b{array_a};
+  [[ maybe_unused ]] std::array<units::nanosecond_t, N> array_b{array_a};
 
   wpi::print("{}\n", array_b[0]);
 }
