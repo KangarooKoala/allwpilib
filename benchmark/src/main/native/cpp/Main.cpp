0f7a16a63073730c6aa007115c010204af6cfeac
Joseph Eng <91924258+KangarooKoala@users.noreply.github.com>
2025-09-04 12:07:20 -0700

    Translate benchmark

--- benchmark/src/main/native/cpp/Main.cpp
+++ benchmark/src/main/native/cpp/Main.cpp
@@ -5,22 +5,24 @@
 #include <benchmark/benchmark.h>
 #include <frc/geometry/Pose2d.h>
 #include <frc/path/TravelingSalesman.h>
+#include <frc/units.h>
 
-#include <units/angle.h>
-#include <units/length.h>
 #include <wpi/array.h>
 
 static constexpr wpi::array<frc::Pose2d, 6> poses{
-    frc::Pose2d{-1_m, 1_m, -90_deg}, frc::Pose2d{-1_m, 2_m, 90_deg},
-    frc::Pose2d{0_m, 0_m, 0_deg},    frc::Pose2d{0_m, 3_m, -90_deg},
-    frc::Pose2d{1_m, 1_m, 90_deg},   frc::Pose2d{1_m, 2_m, 90_deg},
+    frc::Pose2d{-1.0 * mp::m, 1.0 * mp::m, -90.0 * mp::deg},
+    frc::Pose2d{-1.0 * mp::m, 2.0 * mp::m, 90.0 * mp::deg},
+    frc::Pose2d{0.0 * mp::m, 0.0 * mp::m, 0.0 * mp::deg},
+    frc::Pose2d{0.0 * mp::m, 3.0 * mp::m, -90.0 * mp::deg},
+    frc::Pose2d{1.0 * mp::m, 1.0 * mp::m, 90.0 * mp::deg},
+    frc::Pose2d{1.0 * mp::m, 2.0 * mp::m, 90.0 * mp::deg},
 };
 static constexpr int iterations = 100;
 
 void BM_Transform(benchmark::State& state) {
   frc::TravelingSalesman traveler{[](auto pose1, auto pose2) {
     auto transform = pose2 - pose1;
-    return units::math::hypot(transform.X(), transform.Y()).value();
+    return mp::value(mp::hypot(transform.X(), transform.Y()));
   }};
   for (auto _ : state) {
     traveler.Solve(poses, iterations);
@@ -31,7 +33,7 @@ BENCHMARK(BM_Transform);
 void BM_Twist(benchmark::State& state) {
   frc::TravelingSalesman traveler{[](auto pose1, auto pose2) {
     auto twist = (pose2 - pose1).Log();
-    return units::math::hypot(twist.dx, twist.dy).value();
+    return mp::value(mp::hypot(twist.dx, twist.dy));
   }};
   for (auto _ : state) {
     traveler.Solve(poses, iterations);
