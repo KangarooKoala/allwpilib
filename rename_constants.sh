2c797bc39a0569eddb2dfb549fc6c214361d7f20
Joseph Eng <91924258+KangarooKoala@users.noreply.github.com>
2026-02-05 15:07:13 -0800

    Tmp: wpiutil color

--- rename_constants.sh
+++ rename_constants.sh
@@ -2,4 +2,4 @@
 
 git restore --source HEAD -- $(cat rename_constants_paths)
 python3 rename_constants.py $(cat rename_constants_paths)
-git apply rename_constants_update_ntcore_jinja.patch
+# git apply rename_constants_update_ntcore_jinja.patch
